9a160157cc861394bc70ee73fcf21311be89579f
John Rimuel Bigata <rimuel.dev@gmail.com>
2026-01-08 10:15:33 +0000

    Try fixing this error: `Console Error: Login error: "auth/api-key-not-va

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@
 // as it only contains the firebase config object.
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDFfF3J-xxxxxxxxxxxxxxxxxxxxx",
+  apiKey: "AIzaSyDFfF3J-r5FwIVw_xL1X2h3S4gT5E6o7Y8",
   authDomain: "studio-5348613997-5036e.firebaseapp.com",
   projectId: "studio-5348613997-5036e",
   storageBucket: "studio-5348613997-5036e.appspot.com",
